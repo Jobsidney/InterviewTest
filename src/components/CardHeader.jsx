b96c4e28a50c5f03631dff456dfc4d633e30cc12
Jobsidney <jobsidney67@gmail.com>
2024-07-17 01:50:16 +0300

    image

--- src/components/CardHeader.jsx
+++ src/components/CardHeader.jsx
@@ -5,7 +5,7 @@ function CardHeader({title}) {
     <div className="col-lg-8 mx-auto text-center mb-md-5">
     <img
            className=" object-cover" width={"80px"}
-           src={'/src/Images/logo.png'}
+           src='https://pbs.twimg.com/profile_images/1489569110040141826/ZzZgytR8_400x400.png'
            alt="logo"
          />
          <p>{title}</p>
